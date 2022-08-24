1f63152ae12327fb334fd5491ca6187af822bc4d
BenMelton <111529613+BenMelton@users.noreply.github.com>
2022-08-24 12:57:58 -0600

    Update streamlit_app.py
    1st try to add field to existing fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,11 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("insert into fruit_load_list (FRUIT_NAME) 
+               value add_my_fruit")              
+streamlit.write('Thanks for adding ', add_my_fruit)
