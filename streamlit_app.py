2d39fb415dd47e8d8d52ef26d153fc0b8b63f28b
BenMelton <111529613+BenMelton@users.noreply.github.com>
2022-08-25 16:22:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
-streamlit.stop()  
+  
 #New Section to dsiplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -44,7 +44,7 @@ try:
 #test                       
 
 
-streamlit.header("The fruit load list contains:")
+        streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
